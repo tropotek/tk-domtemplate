ff7243ff8952ad9ea7e0bba1fd4aedd49e21a116
tropotek <info@tropotek.com>
2017-06-20 07:34:05 +1000

    Minor Code Updates - 252s-dev.vet.unimelb.edu.au

--- Dom/Loader/Adapter/ClassPath.php
+++ Dom/Loader/Adapter/ClassPath.php
@@ -2,7 +2,6 @@
 namespace Dom\Loader\Adapter;
 
 use \Dom\Template;
-use \Dom\Loader;
 
 /**
  * This adapter will look for template files in the supplied
@@ -32,7 +31,7 @@ class ClassPath extends Iface
 
     /**
      *
-     * @param array $path   The path to the template folder
+     * @param string $path   The path to the template folder
      * @param string $ext   The template file extension, default 'xml'
      */
     public function __construct($path, $ext = 'xml')
@@ -44,8 +43,8 @@ class ClassPath extends Iface
     /**
      * Load an xml/xhtml strings
      *
-     * @param $xhtml
-     * @param $class
+     * @param string $xhtml
+     * @param string $class
      * @return Template
      */
     public function load($xhtml, $class)
@@ -56,16 +55,16 @@ class ClassPath extends Iface
     /**
      * Load an xml/xhtml file
      *
-     * @param $path
-     * @param $class
+     * @param string $path
+     * @param string $class
      * @return Template|null
      */
     public function loadFile($path, $class)
     {
         $class = trim(str_replace('\\', '_', $class), '_');
-        $tplpath = $this->path . '/' . $class . '.' . $this->ext;
-        if (is_file($tplpath)) {
-            return Template::loadFile($tplpath);
+        $tplPath = $this->path . '/' . $class . '.' . $this->ext;
+        if (is_file($tplPath)) {
+            return Template::loadFile($tplPath);
         }
     }
 
